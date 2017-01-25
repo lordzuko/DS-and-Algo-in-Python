3e6167d8aca9f53e6a1c7d88b8e259b67c492c04
Himanshu Maurya <himanshumaurya2214225@hotmail.com>
2017-01-25 13:33:59 +0530

    Implemented shorted path tree

--- Graphs/DijkstraSSSP.py
+++ Graphs/DijkstraSSSP.py
@@ -37,3 +37,19 @@ def shortest_path_length(g, src):
                     pq.update(pqlocator[v], d[v], v)    # update the pq entry
 
     return cloud                            # only includes reachable vertices
+
+def shortest_path_tree(g, s, d):
+    """Reconstruct shortest-path tree rooted at vertex s, given distance map d.
+
+    Return tree as a map from each reachable vertex v (other than s) to the edge
+    e = (u,v) that is used to reach v from its parent u in the tree.
+    """
+    tree = {}
+    for v in d:
+        if v is not s:
+            for e in g.incident_edges(v, False):                # consider incoming edges
+                u = e.opposite(v)
+                wgt = e.element()
+                if d[v] == d[u] + wgt:
+                    tree[v] = e                                 # edge e is used to reach v
+    return tree
